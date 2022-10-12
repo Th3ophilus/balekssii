4fdf8532619c53404c4ed486dd376c375c49b75a
Th3ophilus <112083771+Th3ophilus@users.noreply.github.com>
2022-10-12 08:07:12 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const hamburger = document.getElementById("hamburger")
+const navMenu = document.getElementById("nav-menu")
+
+hamburger.addEventListener("click", ()=>{
+    navMenu.classList.toggle("active")
+})
+
+
+
+
+
+
